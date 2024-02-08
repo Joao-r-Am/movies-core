ea6ec7d65b99c1267a1a3461c01bf66bffa26edb
Joao-r-Am <joaoric.amorim@gmail.com>
2024-02-08 11:40:15 -0300

    movie commit 1

---
+++ src/test/java/dev/joaor/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaor.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
